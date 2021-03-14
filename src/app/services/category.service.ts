8810ccae6de40a63dbed8f0e2032026ea3d15b7a
krack16 <47774247+krack16@users.noreply.github.com>
2021-03-14 13:04:26 +0300

    14321

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/ListResponseModel';
+import { Category } from '../models/category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  apiUrl="https://localhost:44314/api/categories/getall";
+  constructor(private httpClient:HttpClient) { }
+
+  getCategories():Observable<ListResponseModel<Category>>{
+    return this.httpClient.get<ListResponseModel<Category>>(this.apiUrl);
+    
+  }
 }
